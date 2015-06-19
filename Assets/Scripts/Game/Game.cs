86a2369c95675570af76a18b0693ad4ba6157bb1
Bastrakov Sergey <bastrakov.sergei@gmail.com>
2015-06-19 23:36:30 +0300

    Сборка тестовой сцены

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -3,12 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Assets.Scripts.Common;
+using Assets.Scripts.Controllers;
+using Assets.Scripts.Interfaces;
+using Assets.Scripts.Spaceship;
+using Assets.Scripts.UI;
 using UnityEngine;
 
 namespace Assets.Scripts.Game
 {
 	public class Game : Singleton<Game>
-	{
+    {
+        [SerializeField]
+        private SpaceshipBehaviour spaceshipPrefab;
 
+        [SerializeField]
+        private MenuUI menuUI;
+
+	    public MenuUI MenuUI
+	    {
+	        get { return menuUI ?? (menuUI = FindObjectOfType<MenuUI>()); }
+	    }
+
+        private SpaceshipBehaviour spaceshipInstance;
+
+	    public void Start()
+        {
+            spaceshipInstance = Instantiate(spaceshipPrefab);
+
+            MenuUI.ShowUIControllers(spaceshipInstance.GetComponent<ISpaceshipMoveable>());
+	    }
 	}
 }
---
+++ Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Scripts.Controllers;
+using Assets.Scripts.Interfaces;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.UI
+{
+	public class MenuUI : UIBehaviour
+    {
+        [SerializeField]
+        private SpaceshipControllerUI controllerUIPrefab;
+
+        private SpaceshipControllerUI controllerUIInstance;
+
+	    protected SpaceshipControllerUI ControllerUIInstance
+	    {
+	        get
+            {
+                if (controllerUIInstance == null)
+                {
+                    controllerUIInstance = Instantiate(controllerUIPrefab);
+                    controllerUIInstance.transform.parent = transform;
+                    controllerUIInstance.transform.position = Vector3.zero;
+
+                    controllerUIInstance.gameObject.SetActive(false);
+                }
+
+                return controllerUIInstance;
+            }
+	    }
+
+        public void ShowUIControllers(ISpaceshipMoveable moveable)
+	    {
+            ControllerUIInstance.SpaceshipMoveable = moveable;
+            ControllerUIInstance.gameObject.SetActive(true);
+        }
+
+        public void HideUIControllers()
+	    {
+            ControllerUIInstance.gameObject.SetActive(false);
+	    }
+	}
+}
