be1eb694677419bf076909bcdb24e58ff5d0b89e
kirill-fazliev <flame_xxx@inbox.ru>
2015-06-27 15:56:33 +0300

    Добавлены звук двигателя, скрипт звуков двигателя

---
+++ Assets/Scripts/Spaceship/SpaceShipSounds.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Interfaces;
+
+namespace Assets.Scripts.Spaceship
+{
+	public class SpaceShipSounds : MonoBehaviour 
+	{
+		/** Настройка частоты звука работающего двигателя*/
+		[SerializeField] 
+		private float configPitchMultiplier	= 1.0f;
+		/** минимальная частотота звука работающего двигателя*/
+		[SerializeField]
+		private float configMinPitchMultiplier	= 0.3f;
+		[SerializeField]
+		private float configMaxPitchMultiplier	= 1.0f;
+
+		/** Настройка громкости звука работающего двигателя*/
+		[SerializeField]
+		private float configVolumeMultiplier = 1.0f;
+		/** минимальная частотота звука работающего двигателя*/
+		[SerializeField]
+		private float configMinVolumeMultiplier	= 0.3f;
+		[SerializeField]
+		private float configMaxVolumeMultiplier	= 1.0f;
+
+
+		private AudioSource engineAudioSource;
+		private ISpaceshipMoveable spaceshipMoveable;
+		void Start () 
+		{
+			engineAudioSource = GetComponent<AudioSource> ();
+			spaceshipMoveable = Game.PlayerSpaceship.GetComponent<ISpaceshipMoveable> ();
+		}
+		void Update () 
+		{
+			if (spaceshipMoveable.ThrottleLevel == 0.0f) 
+			{
+				engineAudioSource.volume = 0.0f;
+				return;
+			}
+
+			engineAudioSource.pitch = Mathf.Clamp(spaceshipMoveable.ThrottleLevel * configPitchMultiplier, configMinPitchMultiplier, configMaxPitchMultiplier) ;
+			engineAudioSource.volume = Mathf.Clamp(spaceshipMoveable.ThrottleLevel * configVolumeMultiplier, configMinVolumeMultiplier, configMaxVolumeMultiplier) ;;
+		}
+	}
+}
