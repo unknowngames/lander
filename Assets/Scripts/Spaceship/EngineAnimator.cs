73bcae8ac1a034ca4a89ec1898dce0516856abc6
sergey-bastrakov <bastrakov.sergey@gmail.com>
2015-07-04 14:59:14 +0300

    Добавлен эффект двигателя

---
+++ Assets/Scripts/Spaceship/EngineAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Spaceship;
+
+public class EngineAnimator : MonoBehaviour 
+{
+    [SerializeField]
+    private SpaceshipBehaviour spaceship;
+
+    [SerializeField]
+    private ParticleEmitter emitter;
+
+    public void Update ()
+    {
+        emitter.maxEmission = spaceship.EnginePower * 10000.0f;
+    }   
+}
