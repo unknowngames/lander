fbadda65ef6bd8c879c98265f6c1815b0e1d2f5e
sergey-bastrakov <bastrakov.sergey@gmail.com>
2015-07-11 13:55:48 +0300

    Установка родителя в null объекту SpaceshipGhost при взрыве

--- Assets/Scripts/Spaceship/SpaceshipGhost.cs
+++ Assets/Scripts/Spaceship/SpaceshipGhost.cs
@@ -4,13 +4,22 @@ namespace Assets.Scripts.Spaceship
 {
     public class SpaceshipGhost : MonoBehaviour
     {
+        private Transform cashedParent;
+
+        public void Awake ()
+        {
+            cashedParent = transform.parent;
+        }
+
         public void BlowUp()
         {
+            transform.parent = null;
             gameObject.SetActive(true);
         }
 
         public void Reset()
         {
+            transform.parent = cashedParent;
             gameObject.SetActive(false);
         }
     }
