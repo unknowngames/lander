5d5822c358b503f91545781e888048a46b2aa6ff
sergey-bastrakov <bastrakov.sergey@gmail.com>
2015-07-17 09:07:30 +0300

    Скрипты для фиксации посадки

---
+++ Assets/Scripts/Common/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public bool IsTriggered=false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        IsTriggered = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        IsTriggered = false;
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+
+    }  
+}
---
+++ Assets/Scripts/Spaceship/TouchdownTrigger.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TouchdownTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private Trigger[] triggers;  
+    [SerializeField]
+    private float touchdownTimeout = 2.0f;
+
+    public bool Landed = false;
+
+    private Coroutine touchdownCoroutine;
+    private bool isTouchdownTimerStarted = false;
+
+    private bool IsAllTriggered ()
+    {
+        bool result = true;
+
+        foreach (Trigger trigger in triggers)
+        {
+            result = result && trigger.IsTriggered;
+        }
+
+        return result;
+    }
+
+	void Update ()
+    {
+        if (IsAllTriggered() && !isTouchdownTimerStarted)
+	    {
+	        BeginTouchdownTimer ();
+	    }
+        else if (!IsAllTriggered())
+	    {
+	        Landed = false;
+	        StopTouchdownTimer ();
+	    }
+	}
+
+    private void BeginTouchdownTimer()
+    {
+        StopTouchdownTimer ();
+        isTouchdownTimerStarted = true;
+        touchdownCoroutine = StartCoroutine(TouchdownCoroutine());
+    }
+
+    private void StopTouchdownTimer ()
+    {
+        if (touchdownCoroutine != null)
+        {
+            isTouchdownTimerStarted = false;
+            StopCoroutine (touchdownCoroutine);
+        }
+    }
+
+    private IEnumerator TouchdownCoroutine()
+    {
+        yield return new WaitForSeconds (touchdownTimeout);
+        Landed = true;
+        StopTouchdownTimer ();
+    }
+}
