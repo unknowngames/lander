390a77700d94673be0fd79b84c25d045d934cc7d
sergey-bastrakov <bastrakov.sergei@gmail.com>
2015-07-25 11:08:47 +0300

    Порядок отрисовки меню

---
+++ Assets/Scripts/UI/LayoutOrder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LayoutOrder : UIBehaviour
+{
+    [SerializeField]
+    private int order;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        transform.SetSiblingIndex(order);
+    }
+}
