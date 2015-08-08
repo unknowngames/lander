52e3991ae9ce3a3a982df836ba4cd1bbbabf9ea7
sergey-bastrakov <bastrakov.sergei@gmail.com>
2015-08-08 13:14:59 +0300

    Движение фона за кораблем

---
+++ Assets/Scripts/Common/BackgroundFolower.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts;
+
+public class BackgroundFolower : MonoBehaviour
+{
+    public Transform Target;
+
+    [SerializeField]
+    private float xOffset = 750;
+    [SerializeField]
+    private float yOffset = 50;
+                
+
+    public void Update()
+    {
+        if (GameHelper.PlayerSpaceship != null)
+        {
+            Target = GameHelper.PlayerSpaceship.transform;
+        }
+
+        if (Target == null)
+        {
+            return;
+        }
+
+        Vector3 position = transform.position;
+
+        position.x = Target.position.x + xOffset;
+        position.y = Target.position.y + yOffset;
+
+        transform.position = position;
+    }
+}
