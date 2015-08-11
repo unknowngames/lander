78038fe038d1a602dd63c8a790c01275b9410d82
Bastrakov Sergey <bastrakov.sergei@gmail.com>
2015-08-11 23:08:03 +0300

    Анимированая менюшка

---
+++ Assets/Scripts/UI/SpaceButtonAnimator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SpaceButtonAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private UIBehaviour uiBehaviour;
+
+    public void SetExpanded(bool value)
+    {
+        animator.SetBool("expanded", value);
+    }
+}
