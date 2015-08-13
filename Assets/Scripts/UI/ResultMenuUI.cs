a95c1fd95ce4e59fba5bd6f7d35e6f225794e68f
tzardino <lespaul@live.ru>
2015-08-14 00:34:50 +0300

    добавлены текстовые поля на экране результата

--- Assets/Scripts/UI/ResultMenuUI.cs
+++ Assets/Scripts/UI/ResultMenuUI.cs
@@ -7,6 +7,22 @@ namespace Assets.Scripts.UI
 {
     public class ResultMenuUI : MenuUI
     {
+        public UnityEngine.UI.Text SuccessLandingScoreLabel;
+        public UnityEngine.UI.Text SuccessLandingScore;
+
+        public UnityEngine.UI.Text SoftLandingScoreLabel;
+        public UnityEngine.UI.Text SoftLandingScore;
+
+        public UnityEngine.UI.Text PreciseLandingScoreLabel;
+        public UnityEngine.UI.Text PreciseLandingScore;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            SuccessLandingScore.text = Game.Instance.CurrentScore.ScorePoints.ToString();
+        }
+
         public void OnStart()
         {      
             GameHelper.Begin ();
