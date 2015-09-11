5f0667fa3e0b1459c828e87ccc5201fbb4e1605f
tzardino <lespaul@live.ru>
2015-09-11 16:05:37 +0300

    дискретность подсчета очков

--- Assets/Scripts/Session/ScoreCalculator.cs
+++ Assets/Scripts/Session/ScoreCalculator.cs
@@ -40,47 +40,60 @@ namespace Assets.Scripts.Session
             {
                 int totalScore = 0;
 
+                // расчет бонуса за время посадки
+
                 // расчет бонуса за мягкую посадку
-                int softLandingScore = 0;
+                float softLandingScore = 0;
 
                 var collisions = game.PlayerSpaceship.Collisions;
                 bool isLandingSoft = true;
+                const float softCollisionVelocity = 0.5f;
+                float maxSoftCollision = 0.0f;
                 
+
                 foreach(var c in collisions)
                 {
-                    if(c.relativeVelocity.magnitude >= 0.25f)
+                    var magnitude = c.relativeVelocity.magnitude;
+                    if (magnitude >= softCollisionVelocity)
                     {
                         isLandingSoft = false;
                         break;
+                    }
+                    else if(magnitude > maxSoftCollision)
+                    {
+                        maxSoftCollision = magnitude;
                     }
                 }
 
                 if (isLandingSoft)
                 {
-                    softLandingScore += 20;
+                    if(maxSoftCollision <= softCollisionVelocity / 2.0f)
+                        softLandingScore += 20.0f + 20.0f * (1.0f - maxSoftCollision / (softCollisionVelocity / 2.0f));
+                    else if(maxSoftCollision <= softCollisionVelocity)
+                        softLandingScore += 10.0f + 10.0f * (1.0f - maxSoftCollision / softCollisionVelocity);
                 }
 
-                //UnityEngine.Debug.Log("Soft landed : " + isLandingSoft + " collisions: " + collisions.Count);
+                UnityEngine.Debug.Log("Soft landed : " + isLandingSoft + " collisions: " + collisions.Count + " velocity magn: " + maxSoftCollision);
 
                 if (collisions.Count <= 1)
-                    softLandingScore += 20;
+                    softLandingScore += 10;
 
-                current.SoftLandingScore = softLandingScore;
-                totalScore += softLandingScore;
+                current.SoftLandingScore = (int)softLandingScore;
+                totalScore += (int)softLandingScore;
 
                 // расчет очков за точность посадки
-                int preciseLandingScore = 0;
+                float preciseLandingScore = 0;
 
                 float distanceFromCenter = UnityEngine.Mathf.Abs(game.PlayerSpaceship.transform.position.x - game.PlayerSpaceship.TouchdownTrigger.Zone.transform.position.x);
 
                 if (distanceFromCenter <= 0.5f)
-                    preciseLandingScore += 40;
-                else if (distanceFromCenter <= 1.0f)
-                    preciseLandingScore += 20;
+                    preciseLandingScore += 20.0f + 20.0f * (1.0f - distanceFromCenter / 0.5f);
+                else if (distanceFromCenter <= 2.0f)
+                    preciseLandingScore += 10.0f + 10.0f * (1.0f - distanceFromCenter / 2.0f);
 
                 UnityEngine.Debug.Log("Precise landing score: " + preciseLandingScore + " Distance: " + distanceFromCenter);
-                current.PreciseLandingScore = preciseLandingScore;
-                totalScore += preciseLandingScore;
+                current.PreciseLandingScore = (int)preciseLandingScore;
+                totalScore += (int)preciseLandingScore;
 
 
                 // расчет очков за успешную посадку
