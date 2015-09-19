665a4a67e166b6b6ccd4e4fb1aff330069ed2260
sergey-bastrakov <bastrakov.sergei@gmail.com>
2015-09-19 11:37:45 +0300

    Исправлены анимации кнопки и меню. Добавлен InteractableStateBehaviour в анимацию для взаимодействия с меню. Включение/выключение меню при переходе из состояний

--- Assets/Scripts/UI/AnimatedMenu.cs
+++ Assets/Scripts/UI/AnimatedMenu.cs
@@ -1,42 +1,56 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
 
 namespace Assets.Scripts.UI
 {
     [RequireComponent(typeof(Animator))]
     [RequireComponent(typeof(CanvasGroup))]
-    public class AnimatedMenu : MenuUI
+    public class AnimatedMenu : MenuUI, IInteractable
     {
         [SerializeField]
         private Animator animator;
         [SerializeField]
         private CanvasGroup canvasGroup;
 
+        private InteractableStateBehaviour[] interactableStateBehaviours;
+
         public Animator Animator
         {
             get { return animator ?? (animator = GetComponent<Animator>()); }
         }
 
-        public CanvasGroup CanvasGroup
+        private CanvasGroup CanvasGroup
         {
             get { return canvasGroup ?? (canvasGroup = GetComponent<CanvasGroup>()); }
+        }
+
+        public bool Interactable
+        {
+            get { return CanvasGroup.interactable; }
+            set { CanvasGroup.interactable = value; }
+        }
+
+        protected override void Awake()
+        {         
+            base.Awake(); 
+       
+            interactableStateBehaviours = Animator.GetBehaviours<InteractableStateBehaviour>();
+            foreach (InteractableStateBehaviour stateBehaviour in interactableStateBehaviours)
+            {
+                stateBehaviour.InteractableBehaviour = this;
+            }
         }
 
         [ContextMenu("Show")]  
         public override void Show()
         {
-            Animator.SetTrigger("DoShow");
+            Animator.SetBool("IsHidden", false);
         }
 
         [ContextMenu("Hide")]
         public override void Hide()
         {
-            Animator.SetTrigger("DoHide");
-        }
-
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-            Show();
-        }
+            Animator.SetBool("IsHidden", true);
+        }          
     }
 }
---
+++ Assets/Scripts/UI/InteractableStateBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class InteractableStateBehaviour : StateMachineBehaviour
+    {
+        [SerializeField]
+        private bool interactable=false;
+
+        public IInteractable InteractableBehaviour { get; set; }
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+
+            if (InteractableBehaviour != null)
+            {
+                InteractableBehaviour.Interactable = interactable;
+            }
+        }
+    }
+}
