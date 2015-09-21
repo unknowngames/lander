7129ae5b0b159dde575f3bb3c0b5502d5626a151
sergey-bastrakov <bastrakov.sergei@gmail.com>
2015-09-21 19:19:47 +0300

    Концепт круглого меню

---
+++ Assets/Scripts/UI/RadialScroll/ListItem.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Object = System.Object;
+
+namespace Assets.Scripts.UI.RadialScroll
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class ListItem : UIBehaviour
+    {
+        [SerializeField]
+        private RectTransform rectTransform;
+
+        public RectTransform RectTransform
+        {
+            get { return rectTransform ?? (rectTransform = (RectTransform) transform); }
+        }
+    
+        private Object value;
+
+        public Object Value
+        {
+            set
+            {
+                this.value = value;
+            }
+            get
+            {
+                return value;
+            }
+        }
+
+        public OnListItemClickEvent OnClick = new OnListItemClickEvent();
+        public Button Button;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            if (Button != null)
+            {
+                Button.onClick.AddListener(Click);
+            }
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable ();
+            if (Button != null)
+            {
+                Button.onClick.RemoveListener(Click);
+            }
+        }
+
+        protected void Click()
+        {
+            OnClick.Invoke(this);
+        }
+    }
+}
