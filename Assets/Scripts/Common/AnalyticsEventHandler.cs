8c381b244d73f98b5a698388a703d091a2775407
tzardino <lespaul@live.ru>
2015-09-24 22:50:27 +0300

    добавлен обработчик событий аналитики, обработчики подписаны на нажатия кнопок в главном меню

---
+++ Assets/Scripts/Common/AnalyticsEventHandler.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Common
+{
+    public enum AnalyticsHandlerMethod
+    {
+        None,
+        Start,
+        OnDestroy,
+        OnEnable,
+        OnDisable,
+        OnApplicationQuit
+    }
+    public class AnalyticsEventHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private string designEventId;
+
+        [SerializeField]
+        private AnalyticsHandlerMethod method = AnalyticsHandlerMethod.None;
+
+        public void ExecuteEvent()
+        {
+            AnalyticsManager.SendDesignEvent(designEventId);
+        }
+
+        void Start()
+        {
+            if(method == AnalyticsHandlerMethod.Start)
+            {
+                ExecuteEvent();
+            }
+        }
+
+        void OnDestroy()
+        {
+            if (method == AnalyticsHandlerMethod.OnDestroy)
+            {
+                ExecuteEvent();
+            }
+        }
+
+        void OnApplicationQuit()
+        {
+            if (method == AnalyticsHandlerMethod.OnApplicationQuit)
+            {
+                ExecuteEvent();
+            }
+        }
+
+        void OnEnable()
+        {
+            if (method == AnalyticsHandlerMethod.OnEnable)
+            {
+                ExecuteEvent();
+            }
+        }
+
+        void OnDisable()
+        {
+            if (method == AnalyticsHandlerMethod.OnDisable)
+            {
+                ExecuteEvent();
+            }
+        }
+    }
+}
