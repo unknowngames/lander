df075d6834135d0e8774e9f240ede1239f4838e2
tzardino <lespaul@live.ru>
2015-09-30 23:17:55 +0300

    наброски кода для работы с лидербордами

--- Assets/Scripts/Social/Score.cs
+++ Assets/Scripts/Social/Score.cs
@@ -1,21 +1,87 @@
 ﻿using System.Threading.Tasks;
 using Parse;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 namespace Assets.Scripts.Social
 {
     public class Score : MonoBehaviour
     {
-        public void Start ()
-        {
-            ParseObject testObject = new ParseObject("TestObject");
-            testObject["foo"] = "bar";
-            testObject.SaveAsync();
+        [SerializeField]
+        private string trainingLeaderboardName = "TrainingScores";
 
-            Task<ParseUser> task = ParseUser.LogInAsync("login", "password");
-            task.Wait();
-            ParseUser parseUser = task.Result;
-            Debug.Log(parseUser);
+        [SerializeField]
+        private string cadetLeaderboardName = "CadetScores";
+
+        [SerializeField]
+        private string primeLeaderboardName = "PrimeScores";
+
+        [SerializeField]
+        private string commandLeaderboardName = "CommandScores";
+
+        void Start()
+        {
+            UnityEngine.Social.localUser.Authenticate(ProcessAuthentication);
+        }
+
+        public void WriteTrainingRecord(long record)
+        {
+            UnityEngine.Social.ReportScore(record, trainingLeaderboardName, ProcessReportScore);
+        }
+
+        public void WriteCadetRecord(long record)
+        {
+            UnityEngine.Social.ReportScore(record, cadetLeaderboardName, ProcessReportScore);
+        }
+
+        public void WritePrimeRecord(long record)
+        {
+            UnityEngine.Social.ReportScore(record, primeLeaderboardName, ProcessReportScore);
+        }
+
+        public void WriteCommandRecord(long record)
+        {
+            UnityEngine.Social.ReportScore(record, commandLeaderboardName, ProcessReportScore);
+        }
+
+
+        void ProcessAuthentication(bool success)
+        {
+            if(success)
+            {
+                Debug.Log("Authentication success");
+
+                // start read scores
+                UnityEngine.Social.LoadScores(trainingLeaderboardName, ProcessTrainingScores);
+                UnityEngine.Social.LoadScores(cadetLeaderboardName, ProcessCadetScores);
+                UnityEngine.Social.LoadScores(primeLeaderboardName, ProcessPrimeScores);
+                UnityEngine.Social.LoadScores(commandLeaderboardName, ProcessCommandScores);
+            }
+        }
+
+        void ProcessTrainingScores(IScore[] scores)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ProcessCadetScores(IScore[] scores)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ProcessPrimeScores(IScore[] scores)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ProcessCommandScores(IScore[] scores)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        void ProcessReportScore(bool success)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
